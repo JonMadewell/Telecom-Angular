6a90d6fa1ba6e8230959c9ab64ac3c89409980d6
rtayl126 <93009189+rtaylor126@users.noreply.github.com>
2021-11-05 08:37:05 -0400

    ui update

--- src/app/Component/portal/portal.component.ts
+++ src/app/Component/portal/portal.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Devices } from 'src/app/devices.model';
 import { Device } from 'src/app/Models/device.model';
 import { Person } from 'src/app/Models/person.model';
@@ -17,7 +18,9 @@ export class PortalComponent implements OnInit{
  allPlans: Plan []= [];
  plan: Plan= new Plan;
  devices: Device[]= [];
+ pDevice: Device[]=[];
  allDevices: Device []= [];
+ numbers: string []= [];
  device: Device = new Device;
   constructor(private _auth: AuthenticationService, private httpService: HttpClientService) {
    this.person= this._auth.userValue
@@ -33,21 +36,27 @@ export class PortalComponent implements OnInit{
     this.person.account.personId = user.account.personId;
     this.plans= user.account.plans;
     this.devices= user.devices;
-    console.log(this.devices[1])
-
-    this.httpService.getPlans().subscribe((  
-      response: any) => this.handleSuccessfulResponse(response),
+    this.httpService.getNums().subscribe(
+      ( response: any) => this.handleSuccessfulResponseNum(response),
     );
+    this.httpService.getPlans().subscribe(
+      ( response: any) => this.handleSuccessfulResponse(response),
+    );
+    
   }
 
   selectChangeHandler (event: any) {
     //update the ui
     this.plan = this.plans[event.target.value];
+    for(let i =this.plan.deviceLimit; i>=0; i--){
+        this.pDevice[i]= this.devices[i];
+    }
   }
     
+  handleSuccessfulResponseNum(response: string[]){
+    this.numbers= response;
+  }
   handleSuccessfulResponse(response: Plan[]){
     this.allPlans= response;
   }
-  
-  
 }
--- src/app/Services/http-client.service.ts
+++ src/app/Services/http-client.service.ts
@@ -5,23 +5,24 @@ import { Plan } from '../Models/plan.model';
 import { Device} from '../Models/device.model';
 import { Person } from '../Models/person.model';
 import { Observable } from 'rxjs';
+import { AuthenticationService } from './authentication.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HttpClientService {
 
-  constructor( private httpClient: HttpClient) { }
+  constructor( private httpClient: HttpClient, private _auth: AuthenticationService) { }
 
   registerPerson(Person: Person):Observable<Person>{
     return this.httpClient.post<Person>("https://telecomprojectapijmrt.azurewebsites.net/api/people", Person,)
   }
 
-  getUser( UserName: string, Password: string){
+  // getUser( UserName: string, Password: string){
     
-      const headers = new HttpHeaders({Authorization: 'basic' + btoa(UserName + ':' + Password)});
-      return this.httpClient.post<User>("https://telecomprojectapijmrt.azurewebsites.net/api/people/ViewInfo", {headers})
-  }
+  //     const headers = new HttpHeaders({Authorization: 'basic' + btoa(UserName + ':' + Password)});
+  //     return this.httpClient.post<User>("https://telecomprojectapijmrt.azurewebsites.net/api/people/ViewInfo", {headers})
+  // }
 
   getInfo(): Observable<Person>{
     return this.httpClient.get<Person>("https://telecomprojectapijmrt.azurewebsites.net/api/People/ViewInfo")
@@ -39,7 +40,7 @@ export class HttpClientService {
     return this.httpClient.get<Device[]>("https://telecomprojectapijmrt.azurewebsites.net/api/Devices");
   }
 
-  getnum(deviceId: number){
-    return this.httpClient.get<Device>("https://telecomprojectapijmrt.azurewebsites.net/api/Devices");
+  getNums(){
+    return this.httpClient.get<string[]>(`https://telecomprojectapijmrt.azurewebsites.net/api/People/GetPhoneNumbe`);
   }
 }
