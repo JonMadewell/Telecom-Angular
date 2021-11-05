fdb52d5ef7fe2c692aaf0d1c92f49cc95d2c92b4
JonMadewell <jmadewell@skillstorm.com>
2021-11-05 06:59:22 -0500

    tried to add localization

--- src/app/Component/nav-bar/nav-bar.component.ts
+++ src/app/Component/nav-bar/nav-bar.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent implements OnInit {
-
+  lang: any;
   constructor() { }
 
   ngOnInit(): void {
+    this.lang = localStorage.getItem('lang') || 'en'
+  }
+
+  changeLang(lang: any){
+    
+    console.log(lang);
+    localStorage.setItem('lang', lang);
+    window.location.reload();
   }
 
 }
